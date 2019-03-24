872b7e8e09b8ef07b1152ce414b31d249c245e18
Yanny Castrillon <yannyandres117@gmail.com>
2019-03-24 16:58:34 -0500

    Implement Redux basic functionality to add and remove items from your shopping cart.

---
+++ src/store.js
@@ -0,0 +1,22 @@
+import { createStore } from 'redux';
+
+const reducer = (state, action) => {
+  if(action.type === 'ADD_TO_CART') {
+    return {
+      ...state,
+      cart: state.cart.concat(action.payload)
+    }
+  }
+
+  if(action.type === 'REMOVE_TO_CART') {
+    console.log("Remove to cart Action");
+    console.log(state.cart, action.payload);
+    return {
+      ...state,
+      cart: state.cart.filter((e) => e != action.payload)
+    }
+  }
+  return state;
+}
+
+export default createStore(reducer, { cart: [] });
